85459fe6ccdcb838566b31a0e6e4a3b600ee2fc5
lykelly19 <lykelly2014@icloud.com>
2022-03-13 20:15:08 -0400

    refresh page

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -14,15 +14,19 @@ class HomeTableViewController: UITableViewController {
     var tweetArray = [NSDictionary]()
     var numberOfTweet: Int!
     
+    let myRefreshControl = UIRefreshControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loadTweet()
         
+        myRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
     }
     
     
-    func loadTweet(){
+    @objc func loadTweet(){
         
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let myParams = ["count": 10]
@@ -37,6 +41,8 @@ class HomeTableViewController: UITableViewController {
             
             self.tableView.reloadData()
             
+            // stop refreshing once finished loading
+            self.myRefreshControl.endRefreshing()
             
         },
         failure: { (Error) in
